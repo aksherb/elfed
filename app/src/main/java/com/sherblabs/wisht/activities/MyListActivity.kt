5a8cf036bf2825daf6ea84e79594a357cb5ef7e2
Nathan Sherburne <sherbn@amazon.com>
2020-03-28 10:29:52 -0700

    Read and write MyList from Firebase
    Design: use "Map" in Firebase to represent the wish list since the only way to
    update a list in Firebase is to replace the whole thing. But when we use
    a Map we can just use the `database.getReference(KEY).push()` method to
    add an item.
    
    Testing: Run 'app', go to 'My List', add items, view updated list,
    verified the same list is in Firebase

--- app/src/main/java/com/sherblabs/wisht/activities/MyListActivity.kt
+++ app/src/main/java/com/sherblabs/wisht/activities/MyListActivity.kt
@@ -1,34 +1,33 @@
 package com.sherblabs.wisht.activities
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
 import com.sherblabs.wisht.R
 
-class MyListActivity : AppCompatActivity() {
+const val LIST_KEY = "test_firebase_list"
 
-    private var dummyList = listOf<String>("Item1", "Item2", "Item3", "Item4")
+class MyListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_list)
 
-        val adapter = ArrayAdapter(this, R.layout.list_item, getList())
-        val listView = findViewById<ListView>(R.id.listView)
-        listView.adapter = adapter
+        initializeListViewRefresher()
     }
 
-    fun addItem(view: View) {
-        inputPopUp()
+    fun onClickAddItem(view: View) {
+        showAddItemPopUp()
     }
 
-    private fun inputPopUp() {
+    private fun showAddItemPopUp() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle("Item Name")
 
@@ -36,25 +35,35 @@ class MyListActivity : AppCompatActivity() {
         input.inputType = InputType.TYPE_CLASS_TEXT
         builder.setView(input)
 
-        builder.setPositiveButton("OK",
-            DialogInterface.OnClickListener { _, _ ->
-                val updatedList = getList() + listOf<String>(input.text.toString())
-                updateList(updatedList)
-                refreshListView(updatedList)
-            })
-        builder.setNegativeButton("Cancel",
-            DialogInterface.OnClickListener { dialog, _ -> dialog.cancel() })
+        builder.setPositiveButton("OK") { _, _ -> addItemToList(input.text.toString()) }
+        builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
 
         builder.show()
     }
 
-    private fun getList(): List<String> {
-        // TODO: get list from persistent store
-        return dummyList
+    private fun addItemToList(value: String) {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference(LIST_KEY).push()
+        myRef.setValue(value)
     }
 
-    private fun updateList(list: List<String>) {
-        // TODO: update list in persistent store
+    private fun initializeListViewRefresher(){
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference(LIST_KEY)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val map = dataSnapshot.getValue(object : GenericTypeIndicator<Map<String, String>>() {})
+                if (map != null) {
+                    refreshListView(map.values.toList())
+                } else {
+                    Log.w(MyListActivity::class.java.toString(), "Map is null.")
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(MyListActivity::class.java.toString(), "Failed to read value.", error.toException())
+            }
+        })
     }
 
     private fun refreshListView(list: List<String>) {
